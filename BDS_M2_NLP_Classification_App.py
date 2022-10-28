e02fc5bfb13ff477bc5530f7139de69e9dea5ffe
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2022-10-28 12:46:16 +0200

    spacy

--- BDS_M2_NLP_Classification_App.py
+++ BDS_M2_NLP_Classification_App.py
@@ -4,7 +4,7 @@ import numpy as np
 import pickle
 import preprocessor as prepro
 
-import spacy_streamlit as spacy
+import spacy
 nlp = spacy.load('en_core_web_sm')
 
 from sklearn.pipeline import make_pipeline
