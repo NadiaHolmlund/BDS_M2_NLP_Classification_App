a0332c30c24f563ba70121f04ace1becc7193033
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2022-10-28 12:35:11 +0200

    nlp

--- BDS_M2_NLP_Classification_App.py
+++ BDS_M2_NLP_Classification_App.py
@@ -29,7 +29,7 @@ def text_prepro(texts):
 
   clean_container = []
 
-  for text in spacy.(load('en_core_web_sm').pipe(texts_clean, disable=["tagger", "parser", "ner"])):
+  for text in spacy.load('en_core_web_sm').pipe(texts_clean, disable=["tagger", "parser", "ner"])):
 
     txt = [token.lemma_.lower() for token in text 
           if token.is_alpha 
